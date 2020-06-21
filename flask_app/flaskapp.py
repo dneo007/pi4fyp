f1f4963cbc7c600cfda45a1e6e818bbdbe456ac2
Darren Neo <dneo007@e.ntu.edu.sg>
2020-06-22 03:23:57 +0800

    bootstrap integration offline with added CSS

--- flask_app/flaskapp.py
+++ flask_app/flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -19,7 +19,7 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html", posts=posts)
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
