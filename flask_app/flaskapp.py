590b7dded94a94501e3610bae4a87ea0de518d1f
Darren Neo <dneo007@e.ntu.edu.sg>
2020-06-21 22:19:12 +0800

    Updated Templates

--- flask_app/flaskapp.py
+++ flask_app/flaskapp.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Darren Neo',
+        'title' : 'Results',
+        'content': 'First Post Content',
+        'date_posted': 'June 21, 2020'
+    },
+    {
+        'author': 'Darren Neo',
+        'title' : '2nd Result',
+        'content': '2nd Post Content',
+        'date_posted': 'June 22, 2020'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Hello, World!</h1>'
+    return render_template("home.html", posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
