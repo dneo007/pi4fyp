858d739bbc7334b9183c7b9995e83ab3bfe17617
Darren Neo <dneo007@e.ntu.edu.sg>
2020-06-27 17:48:44 +0800

    Application Package Restructuring

--- dbentry.py
+++ dbentry.py
@@ -2,7 +2,7 @@ import sqlite3 as sqlite3
 
 def dbinsert(data):
     try:
-        db = sqlite3.connect('/home/pi/Desktop/flask_app/site.db') #DB connection
+        db = sqlite3.connect('/home/pi/Desktop/flask_app/flaskapp/site.db') #DB connection
         cursor = db.cursor() #Create cursor object
         cursor.execute('''INSERT INTO reading(results) VALUES(?)''', (data,))
         db.commit()
