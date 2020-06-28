4506dda5b1037f1fdbad4b8c55f1116d4b7ac702
Darren Neo <dneo007@e.ntu.edu.sg>
2020-06-28 17:03:05 +0800

    User Authentication

--- flask_app/flaskapp/__init__.py
+++ flask_app/flaskapp/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9b0e29820c277b0c91faa0457f2b2cbb'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view ='login'
+login_manager.login_message_category ='info'
+
 
 from flaskapp import routes
