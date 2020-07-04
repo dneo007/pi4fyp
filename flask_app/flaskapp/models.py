37a5fa9d687d2d4d8d8071bf79776ee763080d79
Darren Neo <dneo007@e.ntu.edu.sg>
2020-07-05 01:33:11 +0800

    Minor Errors

--- flask_app/flaskapp/models.py
+++ flask_app/flaskapp/models.py
@@ -21,7 +21,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(1000),nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
     content = db.Column(db.Text,nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
@@ -33,4 +33,4 @@ class Reading(db.Model):
     results = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"Reading('{self.results}'"
+        return f"Reading('{self.results}')"
