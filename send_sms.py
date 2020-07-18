13151b4d4e51c9ddaa63340e65386c99cd8c0ffe
Darren Neo <dneo007@e.ntu.edu.sg>
2020-07-18 18:41:13 +0800

    modified to replace auth token

--- send_sms.py
+++ send_sms.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "AC7966364dc66cb23c401ae1d3bd78028e"
 # Your Auth Token from twilio.com/console
-auth_token  = "4bf3599560bd1fa2f57486880a62bf15"
+auth_token  = "auth token"
 
 client = Client(account_sid, auth_token)
 
